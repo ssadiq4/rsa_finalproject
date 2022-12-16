44d4590dc9dc7d0b582178192c7ea4d0211bea4c
ssadiq4 <81110201+ssadiq4@users.noreply.github.com>
2022-12-16 15:47:37 -0500

    SUBMITTING

--- RSAProjectController/RSAProjectController.ino
+++ RSAProjectController/RSAProjectController.ino
@@ -1,58 +1,51 @@
 //Robot Sensors/Actuators Final Project
 //James Kaluna, Shayan Sadiq, Andrew Palacio
+//Arduino code for the controller Arduino
 
-//Controller Code
+/*
+This code will need to accomplish the following tasks:
+-Read the two potentiometer knobs from the joystick and convert them into bytes
+-Use the RF transceiver to send the bytes to the peripheral
+-Receive velocity/distance data from the peripheral and display it on the OLED
+*/
 
-//This code will need to accomplish the following tasks:
-//Read the two potentiometer knobs from the joystick and convert them into bytes
-//Use the RF transceiver to send the bytes to the peripheral
-//Receive speed data from the peripheral and display it on the OLED
-
+// including libraries for serial communication, RF transceiver, and OLED display
 #include <SPI.h>
 #include <RH_RF69.h> //RF transceiver library
-#include <Wire.h> 
+#include <Wire.h>
 #include <Adafruit_SSD1306.h>
+
+// initializing SSD1306 display connected using software SPI:
 #define SCREEN_WIDTH 128 // OLED display width, in pixels 
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels 
-
-// Declaration for SSD1306 display connected using software SPI:
-//double check all of your pinouts!
 #define OLED_PICO   6
-#define OLED_CLK    7 
-#define OLED_DC     4 
-#define OLED_CS     3 
-#define OLED_RESET  5 
-Adafruit_SSD1306 display(OLED_PICO, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS); 
+#define OLED_CLK    7
+#define OLED_DC     4
+#define OLED_CS     3
+#define OLED_RESET  5
+Adafruit_SSD1306 display(OLED_PICO, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS); //creates display object
 
-float velo;
-float dist;
 
+// initializing RF transceiver:
 const int CS_pin = 10;
 const int INT_pin = 2;
 const int RST_pin = 9;
-
 RH_RF69 rfTransceiver(CS_pin, INT_pin); //creates transceiver object
 
-//both of these are 1023 analog signals divided by 4 to make a byte
-int LR = 0; //byte signal of steering potentiometer
-int FB = 0; //byte signal of throttle
-int left;
-int right;
 
 void setup() {
-  // put your setup code here, to run once:
 
   // initialize display and set text style and cursor
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
-  display.setTextColor(WHITE); 
+  display.setTextColor(WHITE);
   display.setCursor(10, 10);
-  display.setTextSize(1); 
+  display.setTextSize(1);
 
-  pinMode(RST_pin, OUTPUT);
-  digitalWrite(RST_pin, LOW); //initializing RST pin
+  pinMode(RST_pin, OUTPUT); //initializing RST pin for transceiver
 
-  //Reset the transceiver
+  // Manually reset the transceiver
+  digitalWrite(RST_pin, LOW);
   digitalWrite(RST_pin, HIGH);
   delay(10);
   digitalWrite(RST_pin, LOW);
@@ -72,8 +65,8 @@ void setup() {
   //Set the transceiver frequency
   rfTransceiver.setFrequency(915);
   rfTransceiver.setTxPower(20, true);
-  
-  
+
+
   //x steering potentiometer is A2
   //y throttle potentiometer is A3
   //both potentiometers are neutral at 2.5V
@@ -82,70 +75,72 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //unsigned long start = millis();
-  //we must figure out how to time the transmitting and receiving of signals. 
-  //read the A2 and A3 pins and convert them to bytes
-  LR = analogRead(A2)/4; //voltage signal ranging from 0 to 255
-  FB = analogRead(A3)/4; 
-  FB = (FB - 127) * 2;
+
+  //read the potentiometer signals from the A2 and A3 pins and convert them to bytes
+  int LR = analogRead(A2) / 4; // LR stands for left/right magnitude
+  int FB = analogRead(A3) / 4; // FB stands for forward/backwards magnitude
+  FB = (FB - 127) * 2; // center FB signal at 0 and scale up to -255 - 255 (integer math means this is not going to be exactly symmetrical)
+  int left; //modifier for left motor (minimum (center of joystick) is 0, maximum is 128);
+  int right; //modifier for right motor (minimum (center of joystick) is 0, maximum is 128);
+
+  // if LR >= 128, user is pushing joystick left --> use signal as left modifier
   if (LR >= 128) {
-    left = LR-128;
+    left = LR - 128;
     right = 0;
   }
+  // if LR < 128, user is pushing joystick right --> use signal as right modifier
   if (LR < 128) {
     left = 0;
-    right = 128-LR;
+    right = 128 - LR;
   }
 
-  //these lines transmit the data. We're gonna try 1 character for now.
+  // constrain FB in case integer math above causes 256 value
+  FB = constrain(FB, -255, 255);
+
+  // create character array with left modifier, right modifier, and FB amplitude
   char radiopacket[3] = {left, right, FB};
-  Serial.print(left);
-  Serial.print("     ");
-  Serial.print(right);
-  Serial.print("     ");
-  Serial.println(FB);
-  FB = constrain(FB, 0, 255);
+
+  // send character array using RF transceiver and wait until sent
   rfTransceiver.send((uint8_t *)radiopacket, sizeof(radiopacket));
   rfTransceiver.waitPacketSent();
-//  Serial.println("Packet was sent");
 
-   unsigned long startTime = millis();
-   while (1) {
-     //Serial.println("We're in the while loop");
-     if (rfTransceiver.available()) {
-       uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-       uint8_t len = sizeof(buf);
-       if (rfTransceiver.recv(buf, &len)) {
-//        Serial.println("a packet was received, Pikachu");
-        char radio[] = {1};
-        rfTransceiver.send((uint8_t *)radio, sizeof(radio));
-         buf[len] = 0;
-         velo = buf[0];
-         dist = buf[1];
-         // clear display and reset cursor
-         display.clearDisplay();
-         display.setCursor(0, 0);
-         display.setTextSize(1); 
-         // send the pulse distance to the OLED and display it
-         display.print("Total Dist.: ");
-         display.print(dist);
-         display.println(" m");
+  unsigned long startTime = millis();
+  // create while loop to wait to receive velocity/distance data from peripheral Arduino
+  while (1) {
+    // if transceiver receives message, unpack message into velocity and distance variables
+    if (rfTransceiver.available()) {
+      uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+      uint8_t len = sizeof(buf);
+      if (rfTransceiver.recv(buf, &len)) {
+        buf[len] = 0;
+        float velo = buf[0];
+        float dist = buf[1];
+        // clear display and reset cursor/text size
+        display.clearDisplay();
+        display.setCursor(0, 0);
+        display.setTextSize(1);
+        // send the velocity/distance data to the OLED and display it
+        display.print("Total Dist.: ");
+        display.print(dist);
+        display.println(" m");
+        display.setCursor(0, 15);
+        display.print("Velocity: ");
+        display.setTextSize(2);
+        display.print(velo);
+        display.setCursor(0, 25);
+        display.setTextSize(1);
+        display.println("(cm/s)");
+        display.display();
+        // exit while loop if data message was received and displayed properly
+        break;
 
-         display.setCursor(0, 15);
-  
-         display.print("Velocity: ");
-         display.setTextSize(2); 
-         display.print(velo);
-         display.setCursor(0, 25);
-         display.setTextSize(1); 
-         display.println("(cm/s)");
-         display.display();
-         break;
-         if (millis() - startTime > 500) {
+        // if controller is in the while loop for more than 100 ms, it's because peripheral Arduino is also waiting in a while loop to receive
+        // if that's the case, we want to break the loop somewhere and continue to control the car, so this if statement exits the while loop after 100 ms
+        // and sends joystick data to peripheral Arduino to keep crosstalk active
+        if (millis() - startTime > 100) {
           break;
-         }
-       }   
-     }
-   }
+        }
+      }
+    }
+  }
 }
--- RSAProjectPeripheral/RSAProjectPeripheral.ino
+++ RSAProjectPeripheral/RSAProjectPeripheral.ino
@@ -1,167 +1,151 @@
 //Robot Sensors/Actuators Final Project
 //James Kaluna, Shayan Sadiq, Andrew Palacio
+//Arduino code for the peripheral Arduino
 
-//Arduino code for the peripheral
+/*
+  This code must accomplish the following:
+  -Receive the potentiometer signals from the controller and use it to create bytes for left/right motor speed
+  -Output motor speed data to H-bridge for DC motor contro
+  -Receive inputs from the encoder and calculate velocity and distance.
+  -Send the velocity readings back to the controller
+*/
 
-//This code must accomplish the following:
-//Receive the potentiometer signals from the controller and control DC motor speed
-//Receive inputs from the IMU/GPS and calculate velocity.
-//Send the velocity readings back to the controller
-
+// include libraries for RF transceiver
 #include <SPI.h>
 #include <RH_RF69.h>
-/************ Radio Setup ***************/
-// Change to 434.0 MHz or other frequency,
-// must match the other transceiver's freq!
-#define RF69_FREQ 915.0  //change this!!!!
-#define RFM69_INT 2      //
-#define RFM69_CS 10      //
-#define RFM69_RST 9      //
-// Instance of the radio driver
-RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
-//this code is going to calculate the angular velocity from the left motor encoders and display it on the LED
-volatile bool encoderA = false;  //these two booleans will determine whether the two encoder signals are HIGH or LOW.
-volatile bool encoderB = false;
+// initialize RF transceiver:
+#define RFM69_INT 2
+#define RFM69_CS 10
+#define RFM69_RST 9
+RH_RF69 rf69(RFM69_CS, RFM69_INT); //creates transceiver object
+
+// create global variables for use in ISR that will calculate motor velocity/distance travelled
 volatile long counter = 0;     //will track the relative position of encoder A
-volatile float radius = 3.5;   //radius of wheel in cm
+const float radius = 3.5;   //radius of wheel in cm
 volatile float deltaT;         //time elapsed between every half cycle
 volatile float startTime = 0;  //time in which cycle begins
 volatile float omega;          //angular velocity in rad/s
-volatile float velocity = 1;       //cm/s
-volatile float distance = 0;   //m
+volatile float velocity = 0;   //velocity in cm/s
+volatile float distance = 0;   //distance travelled in m
 
-// pins for H Bridge (assuming EN1 is left motor and EN2 is right motor)
+// pins for H Bridge (input 14 powers input 1 and 4 pins, input 14 powers input 2 and 3 pins, EN1 is enable 1 for left motor and EN2 is enable 2 for right motor)
 const int input14 = 8;
 const int input23 = 4;
-const char EN1 = A1;
+const char EN1 = A0;
 const char EN2 = A5;
 
 void setup() {
-  // put your setup code here, to run once:
+  // initialize Serial Monitor
   Serial.begin(9600);
 
-  attachInterrupt(1, encoderChange, CHANGE);  //this interrupt is triggered when encoder A changes to HIGH or LOW.
-  // attachInterrupt(1, encoderChangeB, CHANGE); //This interrupt is triggered when encoder B changes to HIGH or LOW.
+  //attach ISR to digital pin 3, triggered when encoder signal changes from HIGH to LOW or vice versa
+  attachInterrupt(1, encoderChange, CHANGE);
 
-  pinMode(input14, OUTPUT);  //inputs control motor direction.
-  pinMode(input23, OUTPUT);
-  pinMode(EN1, OUTPUT);  //pwm signal that controls motor speed
-  pinMode(EN2, OUTPUT);  //pwm signal that controls motor speed
+  // initialize H-bridge output pins
+  pinMode(input14, OUTPUT);  //controls motor direction
+  pinMode(input23, OUTPUT);  //controls motor direction
+  pinMode(EN1, OUTPUT);  //pwm signal that controls left motor speed
+  pinMode(EN2, OUTPUT);  //pwm signal that controls right motor speed
+
+  // initialize and manually reset RF transceiver
   pinMode(RFM69_RST, OUTPUT);
-  // manual reset
   digitalWrite(RFM69_RST, LOW);
   delay(100);
   digitalWrite(RFM69_RST, HIGH);
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
+  // display message if RF transceiver initialization was successeful or not
   if (!rf69.init()) {
     Serial.println("RFM69 radio RX init failed");
   }
   else {
     Serial.println("The initialization was successful");
   }
-  if (!rf69.setFrequency(RF69_FREQ)) {
+  if (!rf69.setFrequency(915)) {
     Serial.println("setFrequency failed");
   }
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+  //set RF power
+  rf69.setTxPower(20, true);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int left = 0;
-  int right = 0;
-  int FB = 0;
-  int enableSignalOne;
-  int enableSignalTwo;
+  // initialize variables for controlling motor speed
+  int left = 0; //left motor modifier (0 is center of joystick, 128 is furthest left)
+  int right = 0; //right motor modifier (0 is center of joystick, 128 is furthest right)
+  int FB = 0; //forward/back speed (-255 to 255)
+  int enableSignalOne; //signal output to EN1 pin
+  int enableSignalTwo; //signal output to EN2 pin
   unsigned long startTime = micros();
-  while (micros() - startTime < 500) {
-  char radiopacket[2] = {velocity, distance};
-  //Serial.println("sending");
-  rf69.send((uint8_t *)radiopacket, sizeof(radiopacket));
-  rf69.waitPacketSent();
-  }; 
+  // while loop to wait for transmission from controller Arduino
   while (1) {
-//    Serial.println("in loop");
+    // series of if statements for when peripheral receives message from Arduino
     if (rf69.available()) {
       uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
       uint8_t len = sizeof(buf);
       if (rf69.recv(buf, &len)) {
+        // read message from controller and store in variables
         buf[len] = 0;
         left = buf[0];
         right = buf[1];
         FB = buf[2];
-//        Serial.print(left);
-//        Serial.print("      ");
-//        Serial.print(right);
-//        Serial.print("      ");
-//        Serial.println(FB);
+
+        // if F/B signal is positive, power input pins to move motors forward
         if (FB > 0) {
           digitalWrite(input14, HIGH);
           digitalWrite(input23, LOW);
-        }  // push motor forwards if forward-back signal is positive
+        }
+
+        // if F/B signal is negative, power input pins to move motors backward
         if (FB <= 0) {
           digitalWrite(input14, LOW);
           digitalWrite(input23, HIGH);
-        }  // push motor backwards if forward-back signal is negative
+        }
+
+        // enable signal is equal to forward/back amplitude - left/right modifier (higher modifier means more turning in that direction, so we want motor to go slower)
         enableSignalOne = abs(FB) - left;
         enableSignalTwo = abs(FB) - right;
+        //send enable signal to respective enable piuns
         analogWrite(EN1, enableSignalOne);
         analogWrite(EN2, enableSignalTwo);
+
+        // once message is received, break from while loop
         break;
       }
     }
-    // if (abs(micros() - startTime) > 250000) {break;}
   }
+  // if car is not moving for 250 milliseconds, reset velocity to 0 cm/s.
   if (abs(micros() - startTime) > 250000) {
     velocity = 0;
   }
-  char reception;
 
-  if ((velocity > 0) & (distance > 0)) {
-  //these lines transmit the velocity and distance measurements back to the controller.
-  char radiopacket[2] = {velocity, distance};
-  //Serial.println("sending");
-  rf69.send((uint8_t *)radiopacket, sizeof(radiopacket));
-  //Serial.println(radiopacket);
-  rf69.waitPacketSent();
-  }
-  // if (rf69.available()) {
-  //     uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-  //     uint8_t len = sizeof(buf);
-  //     if (rf69.recv(buf, &len)) {
-  //       buf[len] = 0;
-  //       reception = buf[0]; 
-  //       Serial.println(reception);
-  //       if (reception != NULL) {
-  //         break;
-  //       }
-  //       else {continue;}
-  //       reception = NULL;
-  //     }
-  //   }
-  // }
+  // transmit velocity and distance data to controller Arduino continuously for 10 milliseconds
+  while (micros() - startTime < 10000) {
+    char radiopacket[2] = {velocity, distance};
+    rf69.send((uint8_t *)radiopacket, sizeof(radiopacket));
+    rf69.waitPacketSent();
+  };
+
+
+
 
 }
 
+//ISR that detects change in encoder signal and calculates velocity and distance
 void encoderChange() {
+  // calculate change in time since last ISR activation
   deltaT = micros() - startTime;
   startTime = micros();
+
+  // calculate angular velocity, then convert to linear velocity
   omega = 3.14159 / 120 / (deltaT / 1000000);
   velocity = omega * radius;
 
+  // if a full cycle has completed, increase counter variable 
   if (digitalRead(3) == HIGH) {
     counter++;
   }
-  // if (encoderA != encoderB) { //checks whether both encoders are HIGH or LOW.
-  //   counter++; //if the encoder signals are not equal, then the motor is spinning in a certain direction. counter increases.
-
-  // }
-  // else { //if the encoder signals are equal, the motor is spinning in the opposite direction. counter decreases.
-  //   counter--;
-
-  // }
-  encoderA = !encoderA;                              //changes the boolean to match the measured signal change
   distance = counter * 3.14 * (radius / 100) / 240;  //calculates distance the car has travelled
 }
